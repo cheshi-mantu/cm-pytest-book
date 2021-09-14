371ffe7ede7bf206abc0bb27fd14e19e77152e65
egorivanov <egor.ivanov@pm.me>
2021-09-14 08:11:58 +0300

    added allure related stuff to test_two.py

--- ch1/test_two.py
+++ ch1/test_two.py
@@ -1,6 +1,15 @@
 import allure
+
+@allure.suite("Second steps")
+@allure.story("smoking pytest")
+@allure.feature("tuple assertion")
+@allure.title("Assert a tuple and test will fail")
 def test_failing():
     assert (1, 2, 3) == (3, 2, 1)
 
+@allure.suite("Second steps")
+@allure.story("smoking pytest and assertions")
+@allure.feature("tuple assertion")
+@allure.title("Assert a string and test will fail")
 def test_failing_strings():
     assert "tra-tata" == "trata-ta"
