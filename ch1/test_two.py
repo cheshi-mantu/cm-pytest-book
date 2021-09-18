58ab81be561e19424f41053476821c6260964d86
egorivanov <egor.ivanov@pm.me>
2021-09-18 10:35:15 +0300

    updated test for list wit allure decorators

--- ch1/test_two.py
+++ ch1/test_two.py
@@ -1,15 +1,22 @@
 import allure
 
 @allure.suite("Second steps")
-@allure.story("smoking pytest")
+@allure.story("smoking pytest and assertions with allure report")
 @allure.feature("tuple assertion")
 @allure.title("Assert a tuple and test will fail")
 def test_failing():
     assert (1, 2, 3) == (3, 2, 1)
 
 @allure.suite("Second steps")
-@allure.story("smoking pytest and assertions")
+@allure.story("smoking pytest and assertions with allure report")
 @allure.feature("tuple assertion")
 @allure.title("Assert a string and test will fail")
 def test_failing_strings():
-    assert "tra-tata" == "trata-ta"
+    assert "tra-tata" == "trata-ta"\
+
+@allure.suite("Second steps")
+@allure.story("smoking pytest and assertions with allure report")
+@allure.feature("in assertion")
+@allure.title("Assert a string and test will fail")
+def test_failing_strings():
+    assert 1 in [1,2,3]
