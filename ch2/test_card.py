8c1a96d5b69827544b9d4194711899ccfd7059d0
egorivanov <egor.ivanov@pm.me>
2021-11-05 09:17:47 +0300

    updated .gitignore

--- ch2/test_card.py
+++ ch2/test_card.py
@@ -1,5 +1,7 @@
+import allure
 from cards import Card
 
+allure.epic("chapter2")
 def test_field_access():
     c = Card("something", "brian", "todo", 123)
     assert c.summary == "something"
@@ -7,6 +9,7 @@ def test_field_access():
     assert c.state == "todo"
     assert c.id == 123
 
+allure.epic("chapter2")
 def test_defaults():
     c = Card()
     assert c.summary is None
@@ -14,35 +17,40 @@ def test_defaults():
     assert c.state == "todo"
     assert c.id is None
 
+allure.epic("chapter2")
 def test_equality():
     c1 = Card("something", "brian", "todo", 123)
     c2 = Card("something", "brian", "todo", 123)
     assert c1 == c2
 
+allure.epic("chapter2")
 def test_equality_with_diff_ids():
     c1 = Card("something", "brian", "todo", 123)
     c2 = Card("something", "brian", "todo", 4567)
     assert c1 == c2
 
+allure.epic("chapter2")
 def test_inequality():
     c1 = Card("something", "brian", "todo", 123)
     c2 = Card("completely different", "okken", "done", 123)
     assert c1 != c2
 
+allure.epic("chapter2")
 def test_from_dict():
     c1 = Card("something", "brian", "todo", 123)
-    c2_dict = { "summary": "something",
-                "owner": "brian",
-                "state": "todo",
-                "id": 123 }
+    c2_dict = {"summary": "something",
+               "owner": "brian",
+               "state": "todo",
+               "id": 123}
     c2 = Card.from_dict(c2_dict)
     assert c1 == c2
 
+allure.epic("chapter2")
 def test_to_dict():
     c1 = Card("something", "brian", "todo", 123)
     c2 = c1.to_dict()
-    c2_expected = { "summary": "something",
-                    "owner": "brian",
-                    "state": "todo",
-                    "id": 123 }
+    c2_expected = {"summary": "something",
+                   "owner": "brian",
+                   "state": "todo",
+                   "id": 123}
     assert c2 == c2_expected
